a78317be9eed7a691ae73292d9f1acfa4be35a3c
nivin77789 <nivin77789@gmail.com>
2023-08-08 16:57:30 +0530

    sdaf

--- src/App.js
+++ src/App.js
@@ -253,7 +253,442 @@ function App() {
             </div>
           </div>
         </section>
+
+        <section id="services" className="services sections-bg">
+          <div className="container">
+            {/* data-aos="fade-up" */}
+            <div className="section-header">
+              <h2>Our Services</h2>
+              <p>
+                Aperiam dolorum et et wuia molestias qui eveniet numquam nihil
+                porro incidunt dolores placeat sunt id nobis omnis tiledo stran
+                delop
+              </p>
+            </div>
+
+            <div className="row gy-4">
+              {/* data-aos="fade-up" data-aos-delay="100" */}
+              <div className="col-lg-4 col-md-6">
+                <div className="service-item position-relative">
+                  <div className="icon">
+                    <i className="bi bi-activity"></i>
+                  </div>
+                  <h3>Nesciunt Mete</h3>
+                  <p>
+                    Provident nihil minus qui consequatur non omnis maiores. Eos
+                    accusantium minus dolores iure perferendis tempore et
+                    consequatur.
+                  </p>
+                  <a href="./" className="readmore stretched-link">
+                    Read more <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6">
+                <div className="service-item position-relative">
+                  <div className="icon">
+                    <i className="bi bi-broadcast"></i>
+                  </div>
+                  <h3>Eosle Commodi</h3>
+                  <p>
+                    Ut autem aut autem non a. Sint sint sit facilis nam iusto
+                    sint. Libero corrupti neque eum hic non ut nesciunt dolorem.
+                  </p>
+                  <a href="./" className="readmore stretched-link">
+                    Read more <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6">
+                <div className="service-item position-relative">
+                  <div className="icon">
+                    <i className="bi bi-easel"></i>
+                  </div>
+                  <h3>Ledo Markt</h3>
+                  <p>
+                    Ut excepturi voluptatem nisi sed. Quidem fuga consequatur.
+                    Minus ea aut. Vel qui id voluptas adipisci eos earum
+                    corrupti.
+                  </p>
+                  <a href="./" className="readmore stretched-link">
+                    Read more <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6">
+                <div className="service-item position-relative">
+                  <div className="icon">
+                    <i className="bi bi-bounding-box-circles"></i>
+                  </div>
+                  <h3>Asperiores Commodit</h3>
+                  <p>
+                    Non et temporibus minus omnis sed dolor esse consequatur.
+                    Cupiditate sed error ea fuga sit provident adipisci neque.
+                  </p>
+                  <a href="./" className="readmore stretched-link">
+                    Read more <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6">
+                <div className="service-item position-relative">
+                  <div className="icon">
+                    <i className="bi bi-calendar4-week"></i>
+                  </div>
+                  <h3>Velit Doloremque</h3>
+                  <p>
+                    Cumque et suscipit saepe. Est maiores autem enim facilis ut
+                    aut ipsam corporis aut. Sed animi at autem alias eius
+                    labore.
+                  </p>
+                  <a href="./" className="readmore stretched-link">
+                    Read more <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6">
+                <div className="service-item position-relative">
+                  <div className="icon">
+                    <i className="bi bi-chat-square-text"></i>
+                  </div>
+                  <h3>Dolori Architecto</h3>
+                  <p>
+                    Hic molestias ea quibusdam eos. Fugiat enim doloremque aut
+                    neque non et debitis iure. Corrupti recusandae ducimus enim.
+                  </p>
+                  <a href="./" className="readmore stretched-link">
+                    Read more <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="call-to-action" className="call-to-action">
+          <div className="container text-center">
+            {/* data-aos="zoom-out" */}
+            <a
+              href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+              className="glightbox play-btn"
+            ></a>
+            <h3>Call To Action</h3>
+            <p>
+              {" "}
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
+            <a className="cta-btn" href="./">
+              Call To Action
+            </a>
+          </div>
+        </section>
+        <section id="testimonials" className="testimonials">
+          <div className="container">
+            {/* data-aos="fade-up" */}
+            <div className="section-header">
+              <h2>Testimonials</h2>
+              <p>
+                Voluptatem quibusdam ut ullam perferendis repellat non ut
+                consequuntur est eveniet deleniti fignissimos eos quam
+              </p>
+            </div>
+
+            <div className="slides-3 swiper">
+              {/* data-aos="fade-up"
+              data-aos-delay="100" */}
+              <div className="swiper-wrapper">
+                <div className="swiper-slide">
+                  <div className="testimonial-wrap">
+                    <div className="testimonial-item">
+                      <div className="d-flex align-items-center">
+                        <img
+                          src="assets/img/testimonials/testimonials-1.jpg"
+                          className="testimonial-img flex-shrink-0"
+                          alt=""
+                        />
+                        <div>
+                          <h3>Saul Goodman</h3>
+                          <h4>Ceo &amp; Founder</h4>
+                          <div className="stars">
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p>
+                        <i className="bi bi-quote quote-icon-left"></i>
+                        Proin iaculis purus consequat sem cure digni ssim donec
+                        porttitora entum suscipit rhoncus. Accusantium quam,
+                        ultricies eget id, aliquam eget nibh et. Maecen aliquam,
+                        risus at semper.
+                        <i className="bi bi-quote quote-icon-right"></i>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="swiper-slide">
+                  <div className="testimonial-wrap">
+                    <div className="testimonial-item">
+                      <div className="d-flex align-items-center">
+                        <img
+                          src="assets/img/testimonials/testimonials-2.jpg"
+                          className="testimonial-img flex-shrink-0"
+                          alt=""
+                        />
+                        <div>
+                          <h3>Sara Wilsson</h3>
+                          <h4>Designer</h4>
+                          <div className="stars">
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p>
+                        <i className="bi bi-quote quote-icon-left"></i>
+                        Export tempor illum tamen malis malis eram quae irure
+                        esse labore quem cillum quid cillum eram malis quorum
+                        velit fore eram velit sunt aliqua noster fugiat irure
+                        amet legam anim culpa.
+                        <i className="bi bi-quote quote-icon-right"></i>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="swiper-slide">
+                  <div className="testimonial-wrap">
+                    <div className="testimonial-item">
+                      <div className="d-flex align-items-center">
+                        <img
+                          src="assets/img/testimonials/testimonials-3.jpg"
+                          className="testimonial-img flex-shrink-0"
+                          alt=""
+                        />
+                        <div>
+                          <h3>Jena Karlis</h3>
+                          <h4>Store Owner</h4>
+                          <div className="stars">
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p>
+                        <i className="bi bi-quote quote-icon-left"></i>
+                        Enim nisi quem export duis labore cillum quae magna enim
+                        sint quorum nulla quem veniam duis minim tempor labore
+                        quem eram duis noster aute amet eram fore quis sint
+                        minim.
+                        <i className="bi bi-quote quote-icon-right"></i>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="swiper-slide">
+                  <div className="testimonial-wrap">
+                    <div className="testimonial-item">
+                      <div className="d-flex align-items-center">
+                        <img
+                          src="assets/img/testimonials/testimonials-4.jpg"
+                          className="testimonial-img flex-shrink-0"
+                          alt=""
+                        />
+                        <div>
+                          <h3>Matt Brandon</h3>
+                          <h4>Freelancer</h4>
+                          <div className="stars">
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p>
+                        <i className="bi bi-quote quote-icon-left"></i>
+                        Fugiat enim eram quae cillum dolore dolor amet nulla
+                        culpa multos export minim fugiat minim velit minim dolor
+                        enim duis veniam ipsum anim magna sunt elit fore quem
+                        dolore.
+                        <i className="bi bi-quote quote-icon-right"></i>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="swiper-slide">
+                  <div className="testimonial-wrap">
+                    <div className="testimonial-item">
+                      <div className="d-flex align-items-center">
+                        <img
+                          src="assets/img/testimonials/testimonials-5.jpg"
+                          className="testimonial-img flex-shrink-0"
+                          alt=""
+                        />
+                        <div>
+                          <h3>John Larson</h3>
+                          <h4>Entrepreneur</h4>
+                          <div className="stars">
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                            <i className="bi bi-star-fill"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p>
+                        <i className="bi bi-quote quote-icon-left"></i>
+                        Quis quorum aliqua sint quem legam fore sunt eram irure
+                        aliqua veniam tempor noster veniam enim culpa labore
+                        duis sunt culpa nulla illum cillum fugiat legam esse
+                        veniam culpa fore.
+                        <i className="bi bi-quote quote-icon-right"></i>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="swiper-pagination"></div>
+            </div>
+          </div>
+        </section>
       </main>
+
+      <footer id="footer" className="footer">
+        <div className="container">
+          <div className="row gy-4">
+            <div className="col-lg-5 col-md-12 footer-info">
+              <a href="index.html" className="logo d-flex align-items-center">
+                <span>Impact</span>
+              </a>
+              <p>
+                Cras fermentum odio eu feugiat lide par naso tierra. Justo eget
+                nada terra videa magna derita valies darta donna mare fermentum
+                iaculis eu non diam phasellus.
+              </p>
+              <div className="social-links d-flex mt-4">
+                <a href="./" className="twitter">
+                  <i className="bi bi-twitter"></i>
+                </a>
+                <a href="./" className="facebook">
+                  <i className="bi bi-facebook"></i>
+                </a>
+                <a href="./" className="instagram">
+                  <i className="bi bi-instagram"></i>
+                </a>
+                <a href="./" className="linkedin">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="col-lg-2 col-6 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li>
+                  <a href="./">Home</a>
+                </li>
+                <li>
+                  <a href="./">About us</a>
+                </li>
+                <li>
+                  <a href="./">Services</a>
+                </li>
+                <li>
+                  <a href="./">Terms of service</a>
+                </li>
+                <li>
+                  <a href="./">Privacy policy</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-2 col-6 footer-links">
+              <h4>Our Services</h4>
+              <ul>
+                <li>
+                  <a href="./">Web Design</a>
+                </li>
+                <li>
+                  <a href="./">Web Development</a>
+                </li>
+                <li>
+                  <a href="./">Product Management</a>
+                </li>
+                <li>
+                  <a href="./">Marketing</a>
+                </li>
+                <li>
+                  <a href="./">Graphic Design</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+              <h4>Contact Us</h4>
+              <p>
+                A108 Adam Street New York, NY 535022 United States
+                <strong>Phone:</strong> +1 5589 55488 55
+                <strong>Email:</strong> info@example.com
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="container mt-4">
+          <div className="copyright">
+            &copy; Copyright{" "}
+            <strong>
+              <span>Impact</span>
+            </strong>
+            . All Rights Reserved
+          </div>
+          <div className="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+      </footer>
+
+      <a
+        href="#"
+        class="scroll-top d-flex align-items-center justify-content-center"
+      >
+        <i class="bi bi-arrow-up-short"></i>
+      </a>
+
+      {/* <div id="preloader"></div> */}
+
+      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/aos/aos.js"></script>
+      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+      <script src="assets/vendor/php-email-form/validate.js"></script>
+
+      <script src="assets/js/main.js"></script>
     </div>
   );
 }
