f9a40a31a9502b5f2be226c56d4ef8c799eb3103
nivin77789 <nivin77789@gmail.com>
2023-08-12 22:31:27 +0530

    dfs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import TrackVisibility from "react-on-screen";
+import Marquee from "react-fast-marquee";
 function App() {
   //data-aos
   useEffect(() => {
@@ -60,6 +61,7 @@ function App() {
       setIndex((prevIndex) => prevIndex + 1);
     }
   };
+  const arr = [1, 2, 3, 4, 5, 6, 7];
   return (
     <div className="App">
       <header id="header" className="header d-flex align-items-center">
@@ -104,7 +106,7 @@ function App() {
                     }
                   >
                     <h2>
-                      Welcome to <span>Revolve</span>
+                      Welcome to <span className="text-grad">Revolve</span>
                     </h2>
                     <h1>
                       {`Enhancing Business Decision-Making with`}{" "}
@@ -218,6 +220,16 @@ function App() {
         </div>
       </section>
       <main id="main">
+        <center>
+          <div className="marq">
+            <Marquee className="marq-s">
+              {arr.map((ele) => (
+                <img src={`assets/img/${ele}.png`} alt="" />
+              ))}
+            </Marquee>
+          </div>
+        </center>
+
         <section id="about" className="about">
           <div className="container" data-aos="fade-up">
             <div className="section-header">
@@ -377,6 +389,116 @@ function App() {
             <a className="cta-btn" href="./">
               Call To Action
             </a>
+          </div>
+        </section>
+
+        <section id="contact" className="contact">
+          <div className="container" data-aos="fade-up">
+            <div className="section-header">
+              <h2>Contact</h2>
+              <p>
+                Nulla dolorum nulla nesciunt rerum facere sed ut inventore quam
+                porro nihil id ratione ea sunt quis dolorem dolore earum
+              </p>
+            </div>
+
+            <div className="row gx-lg-0 gy-4">
+              <div className="col-lg-4">
+                <div className="info-container d-flex flex-column align-items-center justify-content-center">
+                  <div className="info-item d-flex">
+                    <i className="bi bi-geo-alt flex-shrink-0"></i>
+                    <div>
+                      <h4>Location:</h4>
+                      <p>A108 Adam Street, New York, NY 535022</p>
+                    </div>
+                  </div>
+
+                  <div className="info-item d-flex">
+                    <i className="bi bi-envelope flex-shrink-0"></i>
+                    <div>
+                      <h4>Email:</h4>
+                      <p>info@example.com</p>
+                    </div>
+                  </div>
+
+                  <div className="info-item d-flex">
+                    <i className="bi bi-phone flex-shrink-0"></i>
+                    <div>
+                      <h4>Call:</h4>
+                      <p>+1 5589 55488 55</p>
+                    </div>
+                  </div>
+                  <div className="info-item d-flex">
+                    <i className="bi bi-clock flex-shrink-0"></i>
+                    <div>
+                      <h4>Open Hours:</h4>
+                      <p>Mon-Sat: 11AM - 23PM</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-8">
+                <form
+                  action="forms/contact.php"
+                  method="post"
+                  role="form"
+                  className="php-email-form"
+                >
+                  <div className="row">
+                    <div className="col-md-6 form-group">
+                      <input
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6 form-group mt-3 mt-md-0">
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        id="email"
+                        placeholder="Your Email"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group mt-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="subject"
+                      id="subject"
+                      placeholder="Subject"
+                      required
+                    />
+                  </div>
+                  <div className="form-group mt-3">
+                    <textarea
+                      className="form-control"
+                      name="message"
+                      rows="7"
+                      placeholder="Message"
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="my-3">
+                    <div className="loading">Loading</div>
+                    <div className="error-message"></div>
+                    <div className="sent-message">
+                      Your message has been sent. Thank you!
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <button type="submit">Send Message</button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </section>
       </main>
