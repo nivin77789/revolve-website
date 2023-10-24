d0fd4658b3736e954cbc8b6741b61bd33cfdeda6
nivin77789 <nivin77789@gmail.com>
2023-10-24 14:57:13 +0530

    sdg

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -56,10 +56,10 @@ function Landing() {
               </div>
             </div>
             <div className="col-lg-6 order-1 order-lg-2 baner-im">
-              <Spline
+              {/* <Spline
                 className="drm"
-                scene="https://prod.spline.design/3ZcBSvEWQoelPJMu/scene.splinecode"
-              />
+                scene="https://prod.spline.design/6UzixtiOjUNL79eq/scene.splinecode"
+              /> */}
               <img
                 src="assets/img/hero.png"
                 className="img-fluid hero-img moveArrow"
